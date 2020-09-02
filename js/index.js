d54cdaf43bd3e1d341f0b15b6f68c070d210b907
Magdalena Schuetz <m.schuetz@outlook.de>
2020-09-02 13:03:51 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,7 +9,4 @@ console.log(name)
 console.log("Height:")
 console.log(height+1)
 
-function happyHolidaysTo(string){
-  var name = "Maggie";
-    (console.log(`'Happy holidays to ${name}'`));
-  }
+console.log(`'Happy holidays to ${name}'`)
